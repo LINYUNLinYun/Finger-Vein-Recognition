bf62575e2e05314be0aa0804239974291dd24653
LINYUNLinYun <1828951982@qq.com>
2024-12-18 18:21:49 +0800

    修改

--- pre_process.py
+++ pre_process.py
@@ -95,15 +95,15 @@ def bgr2binary(img):
     # 将图片转化为灰度图
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 对灰度图做一个高斯滤波
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    # gray = cv2.GaussianBlur(gray, (5, 5), 0)
     # 对灰度图做一个均值滤波
-    # gray = cv2.blur(gray, (5, 5))
+    gray = cv2.blur(gray, (7, 7))
 
     # show_gray_histogram(gray)
     # 二值化
     ret, binary = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY )
     # 先腐蚀后膨胀，核大小为7*7
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((7, 7), np.uint8)
     # binary = cv2.dilate(binary, kernel)
     binary = cv2.erode(binary, kernel)
     binary = cv2.dilate(binary, kernel)
@@ -123,6 +123,24 @@ def find_corners(binary_path = 'PV1_binary/131/1.bmp',draw_enable=False):
         contours_area.append(cv2.contourArea(contour))
     print(contours_area)
     max_contour_index = np.argmax(contours_area)
+    # 找到凸包角点
+    hull=cv2.convexHull(contours[max_contour_index])
+    # 绘制凸包角点
+    cv2.polylines(img, [hull], True, (0, 0, 255), 2)
+    # print(contours[max_contour_index].shape)
+    for i in range(contours[max_contour_index].shape[0]):
+        cv2.circle(img, (contours[max_contour_index][i][0][0], contours[max_contour_index][i][0][1]), 5, (255, 255, 2), -1)
+    # 绘制凸包缺陷点
+    # defects = cv2.convexityDefects(contours[max_contour_index], hull)
+    # print(defects)
+
+    # for i in range(defects.shape[0]):
+    #     s, e, f, d = defects[i, 0]
+    #     start = tuple(contours[0][s][0])
+    #     end = tuple(contours[0][e][0])
+    #     far = tuple(contours[0][f][0])
+    #     cv2.line(img, start, end, [0, 255, 0], 2)
+    #     cv2.circle(img, far, 5, [0, 0, 255], -1)
         
     
     # contour = max(contours, key= lambda x: cv2.contourArea(x))
@@ -137,6 +155,8 @@ def find_corners(binary_path = 'PV1_binary/131/1.bmp',draw_enable=False):
     return img
 
 if __name__ == '__main__':
+    # save_bin_images('PV1')
+    # save_bin_images('PV2')
     # save_bin_images_for_PPPV('PPPV')
     # binary = cv2.imread('PV1_binary/131/1.bmp',cv2.IMREAD_GRAYSCALE)
     img = find_corners('PV1_binary/131/1.bmp', True)
